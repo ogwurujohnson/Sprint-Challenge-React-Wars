a91c20b4d0ef970589217073bfcc21ce98719b6e
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-31 09:20:47 +0100

    Feat: Build out the all character component and render the single character component

--- starwars/src/components/AllCharacters.js
+++ starwars/src/components/AllCharacters.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import SingleCharacter from "./SingleCharacter";
+import "./StarWars.css";
 
-export default function AllCharacters() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function AllCharacters(props) {
+  const { characters } = props;
+  return (
+    <div>
+      <ul>
+        {characters.map((character, index) => {
+          return (
+            <SingleCharacter
+              key={character.name}
+              character={character}
+              index={index}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
